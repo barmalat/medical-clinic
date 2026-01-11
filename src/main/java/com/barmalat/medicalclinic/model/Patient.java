3b1d9c69580e8d1502ee13721b1879ad8c0ad1fc
barmalat <barmalat@yahoo.com>
2026-01-11 20:35:26 +0100

    [MED-03] jpa entities

--- src/main/java/com/barmalat/medicalclinic/model/Patient.java
+++ src/main/java/com/barmalat/medicalclinic/model/Patient.java
@@ -1,5 +1,11 @@
 package com.barmalat.medicalclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,13 +13,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
+@Table(name = "PATIENT")
 public class Patient {
+    @Column(name = "email")
     private String email;
+    @Column(name = "password")
     private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCardNo")
     private String idCardNo;
+    @Column(name = "firstName")
     private String firstName;
+    @Column(name = "lastName")
     private String lastName;
+    @Column(name = "phoneNumber")
     private String phoneNumber;
+    @Column(name = "birthday")
     private String birthday;
 
     public void updatePatientPublicData(Patient patient){
